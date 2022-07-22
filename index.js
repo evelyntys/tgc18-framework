7b66afa7b1d7f65db443ff6c65037cd06c2a4f46
evelyntys <evelyntys01@gmail.com>
2022-07-22 13:20:44 +0800

    added routing to pages

--- index.js
+++ index.js
@@ -13,6 +13,18 @@ app.use(express.static('public'))
 wax.on(hbs.handlebars);
 wax.setLayoutPath('./views/layouts');
 
+// when get req, will match the routes one by one 
+// => match all in landingRoutes first, then change to productRoutes if cannot find
+
+// first arg is the prefix
+const landingRoutes = require('./routes/landing') 
+// => router object is returned into landingRoutes
+app.use('/', landingRoutes)
+
+const productRoutes = require('./routes/products')
+app.use('/products', productRoutes)
+
+
 //enable forms
 app.use(
     express.urlencoded({
@@ -20,15 +32,15 @@ app.use(
     })
 )
 
-async function main() {
+// async function main() {
 
-    app.get('/', async function (req, res) {
-        res.send('hello world')
-    })
+//     app.get('/', async function (req, res) {
+//         res.send('hello world')
+//     })
 
-}
+// }
 
-main();
+// main();
 
 app.listen(3000, function () {
     console.log('server started')
